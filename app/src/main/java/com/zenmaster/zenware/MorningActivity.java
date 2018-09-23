14973b53323bd3cfa364780fd73945f3ebf89248
toumash <mr.toumash@gmail.com>
2018-09-24 01:19:00 +0200

    adds save on morning acivity click

--- app/src/main/java/com/zenmaster/zenware/MorningActivity.java
+++ app/src/main/java/com/zenmaster/zenware/MorningActivity.java
@@ -1,13 +1,47 @@
 package com.zenmaster.zenware;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-public class MorningActivity extends AppCompatActivity {
+import com.zenmaster.zenware.model.MoodEntry;
+import com.zenmaster.zenware.viewmodel.MoodViewModel;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.morning_activity);
+import java.util.List;
+
+public class MorningActivity extends FragmentActivity implements View.OnClickListener {
+
+  Context ctx;
+  private MoodViewModel mWordViewModel;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.morning_activity);
+    ctx = this;
+    findViewById(R.id.morning_save).setOnClickListener(this);
+
+    mWordViewModel = ViewModelProviders.of(this).get(MoodViewModel.class);
+  }
+
+  @Override
+  public void onClick(View v) {
+    if (v.getId() == R.id.morning_save) {
+      MoodEntry mood = new MoodEntry();
+      SeekBar sb = findViewById(R.id.morning_sb_mood);
+      mood.setMoodScore(sb.getProgress());
+      mWordViewModel.insert(mood);
+      Toast.makeText(ctx, "added new mood entry", Toast.LENGTH_SHORT).show();
+      this.finish();
     }
+  }
 }
--- app/src/main/java/com/zenmaster/zenware/activity/MoodEntriesActivity.java
+++ app/src/main/java/com/zenmaster/zenware/activity/MoodEntriesActivity.java
@@ -1,14 +1,9 @@
 package com.zenmaster.zenware.activity;
 
-import android.app.Activity;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.arch.persistence.db.SupportSQLiteDatabase;
-import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -19,13 +14,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.zenmaster.zenware.R;
-import com.zenmaster.zenware.dao.MoodEntryDao;
-import com.zenmaster.zenware.database.ZenAwareDb;
 import com.zenmaster.zenware.model.MoodEntry;
 import com.zenmaster.zenware.viewmodel.MoodViewModel;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -56,6 +48,7 @@ class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolde
 
   private final LayoutInflater mInflater;
   private List<MoodEntry> mWords; // Cached copy of words
+
   WordListAdapter(Context context) {
     mInflater = LayoutInflater.from(context);
   }
@@ -71,7 +64,10 @@ class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolde
     if (mWords != null) {
       MoodEntry current = mWords.get(position);
       SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH);
-      holder.dateView.setText(dt.format(current.getDate()));
+      if (current.getDate() != null)
+        holder.dateView.setText(dt.format(current.getDate()));
+      else
+        holder.dateView.setText("Invalid date");
       holder.sleepView.setText(String.valueOf(current.getSleepHours()));
       holder.scoreView.setText(String.valueOf(current.getMoodScore()));
     } else {
