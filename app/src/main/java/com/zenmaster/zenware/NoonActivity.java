0ef7c7d99a326500c83a148882e48aa3815e0439
Alicja <alizjonizm@gmail.com>
2018-09-23 12:20:00 +0200

    Adds Noon Layout

---
+++ app/src/main/java/com/zenmaster/zenware/NoonActivity.java
@@ -0,0 +1,13 @@
+package com.zenmaster.zenware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.noon_activity);
+    }
+}
